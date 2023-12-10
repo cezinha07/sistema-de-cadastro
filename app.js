7cf1d097c3951e8bc1a730a181905ad1d67d0a2b
cezinha07 <130381085+cezinha07@users.noreply.github.com>
2023-12-10 01:43:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicaçao esta se conectando ao banco de dados")
-// mais um console log de teste :
+// mais um console log de teste:
 console.log("o Paulo fez essa alteraçao enquanto voce nao via a aula"
+console.log("Enviando para o GitHub")
